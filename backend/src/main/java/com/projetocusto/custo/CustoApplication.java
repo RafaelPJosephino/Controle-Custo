f90281cf8f09c7982697ad6087228c0ad2784c41
Rafael P Jpsephino <Rafael.puckoski.josephino@gmail.com>
2021-09-12 00:51:17 -0300

    Project created

---
+++ backend/src/main/java/com/projetocusto/custo/CustoApplication.java
@@ -0,0 +1,13 @@
+package com.projetocusto.custo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetocusto/custo/CustoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocusto.custo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
